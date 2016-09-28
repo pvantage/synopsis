85d7e95789a4a3f861444ddb9432cb6e3c1cc0bb
user <user>
2016-09-28 10:45:43 +0530

    gfd

--- js/dbtables.js
+++ js/dbtables.js
@@ -11,19 +11,20 @@ function onDeviceReady() {
 
 // Populate the database 
 //
-function populateDB(tx) {
-	 //tx.executeSql('DROP TABLE IF EXISTS USERS');
-	 tx.executeSql('CREATE TABLE IF NOT EXISTS NEWS (newid,title,summary,news_url,news_source,published_by,video,image,share_url,post_date,like,readed,bookmark)');
-	 tx.executeSql('CREATE TABLE IF NOT EXISTS NEWSCATEGORY (newid,category)');
-	 tx.executeSql('CREATE TABLE IF NOT EXISTS NEWSSETTINGS (meta_key,meta_value)');
-	 var requestUrl = "http://ip-api.com/json";
 
+function populateDB(tx) {
+	 tx.executeSql('DROP TABLE IF EXISTS NEWS');
+	 tx.executeSql('CREATE TABLE IF NOT EXISTS NEWS (newid INTEGER,title TEXT,summary LONGTEXT,news_url TEXT,news_source TEXT,published_by TEXT,video TEXT,image TEXT,share_url TEXT,post_date DATETIME,like INTEGER,readed INTEGER,bookmark INTEGER)');
+	 tx.executeSql('CREATE TABLE IF NOT EXISTS NEWSCATEGORY (newid INTEGER,category TEXT)');
+	 tx.executeSql('CREATE TABLE IF NOT EXISTS NEWSSETTINGS (meta_key TEXT,meta_value TEXT)');
+	 alert('start');
+	var requestUrl = "http://ip-api.com/json";
 	jQuery.ajax({
 	  url: requestUrl,
 	  type: 'GET',
 	  success: function(json)
 	  {
-			
+			alert(json.countryCode);
 			tx.executeSql("SELECT * FROM NEWSSETTINGS where meta_key='countrycode'", [],
 			function(tx,results){
 				
@@ -52,7 +53,7 @@ function populateDB(tx) {
 // Transaction error callback
 //
 function errorCB(tx, err) {
-	//alert("Error processing SQL: "+err);
+	alert("Error processing SQL: "+err);
 }
 
 // Transaction success callback
