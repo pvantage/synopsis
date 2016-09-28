3f99c0098b909be691348c158c1cfdafb27f6bac
user <user>
2016-09-28 10:49:02 +0530

    gf

--- js/dbtables.js
+++ js/dbtables.js
@@ -17,17 +17,17 @@ function populateDB(tx) {
 	 tx.executeSql('CREATE TABLE IF NOT EXISTS NEWS (newid INTEGER,title TEXT,summary LONGTEXT,news_url TEXT,news_source TEXT,published_by TEXT,video TEXT,image TEXT,share_url TEXT,post_date DATETIME,like INTEGER,readed INTEGER,bookmark INTEGER)');
 	 tx.executeSql('CREATE TABLE IF NOT EXISTS NEWSCATEGORY (newid INTEGER,category TEXT)');
 	 tx.executeSql('CREATE TABLE IF NOT EXISTS NEWSSETTINGS (meta_key TEXT,meta_value TEXT)');
-	 alert('start');
+	 //alert('start');
 	var requestUrl = "http://ip-api.com/json";
 	jQuery.ajax({
 	  url: requestUrl,
 	  type: 'GET',
 	  success: function(json)
 	  {
-			alert(json.countryCode);
+			
 			tx.executeSql("SELECT * FROM NEWSSETTINGS where meta_key='countrycode'", [],
 			function(tx,results){
-				
+				alert(json.countryCode);
 				if(typeof results.rows.item(0).meta_value != 'undefined'){
 					var sql="UPDATE NEWSSETTINGS SET meta_value='"+json.countryCode+"' WHERE meta_key='countrycode'";
 					tx.executeSql(sql,[],function(){},errorCB);
@@ -39,8 +39,7 @@ function populateDB(tx) {
 					tx.executeSql(sql,[],function(){},errorCB);
 					alert(json.countryCode);
 				}
-			}
-			, errorCB);
+			}, errorCB);
 			
 	  },
 	  error: function(err)
