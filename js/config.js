0132d9eee966b0312c9de40d96dad68081622bdb
user <user>
2016-10-05 17:06:09 +0530

    hgf

--- js/config.js
+++ js/config.js
@@ -21,7 +21,7 @@ function sitemenus()
 	if(uid!='undefined' && typeof uid!='undefined'){
 		
 		if(localStorage.getItem('displayname')!=null){
-			if(localStorage.getItem('displayimg')!=null){
+			if(localStorage.getItem('displayimg')!=null && localStorage.getItem('displayimg')!=''){
 				loginlink='<li><a href="index.html" class="closedaccountmenu"><div class="cat_img"><img src="images/accountimage.png"/></div>Account <span class="setting_right_icon"><img src="images/expendsarrow.png"/></span></a><ul class="social-media activeprofile" style="display:none;"><li><div class="social_img gmail">'+localStorage.getItem('displayimg')+' '+localStorage.getItem('displayname')+'</div><a href="index.html">Logout</a></li></ul></li>';
 			}
 			else
